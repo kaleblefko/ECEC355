84b85239c2dc00cd3de3352e93a3da2d9b0f794e
Kaleb <104028332+kaleblefko@users.noreply.github.com>
2024-02-17 13:39:06 -0500

    Added parser to project 2

--- project_2_3_4_5/Parser.c
+++ project_2_3_4_5/Parser.c
@@ -37,7 +37,25 @@ void loadInstructions(Instruction_Memory *i_mem, const char *trace)
         {
             parseRType(raw_instr, &(i_mem->instructions[IMEM_index]));
             i_mem->last = &(i_mem->instructions[IMEM_index]);
-	}
+        }else if(strcmp(raw_instr, "addi") == 0 ||
+            strcmp(raw_instr, "subi") == 0 ||
+            strcmp(raw_instr, "slli") == 0 ||
+            strcmp(raw_instr, "srli") == 0 ||
+            strcmp(raw_instr, "xori") == 0 ||
+            strcmp(raw_instr, "ori")  == 0 ||
+            strcmp(raw_instr, "andi") == 0 ||
+            strcmp(raw_instr, "ld") == 0) 
+
+        {
+            parseIType(raw_instr, &(i_mem->instructions[IMEM_index]));
+            i_mem->last = &(i_mem->instructions[IMEM_index]);
+	    }else if(strcmp(raw_instr, "bne") == 0)
+        {
+            parseSBType(raw_instr, &(i_mem->instructions[IMEM_index]));
+            i_mem->last = &(i_mem->instructions[IMEM_index]);
+        }else{
+            continue;
+        }
 
         IMEM_index++;
         PC += 4;
@@ -58,6 +76,36 @@ void parseRType(char *opr, Instruction *instr)
         opcode = 51;
         funct3 = 0;
         funct7 = 0;
+    }else if (strcmp(opr, "sub") == 0)
+    {
+        opcode = 51;
+        funct3 = 0;
+        funct7 = 32;
+    }else if (strcmp(opr, "sll") == 0)
+    {
+        opcode = 51;
+        funct3 = 1;
+        funct7 = 0;
+    }else if (strcmp(opr, "srl") == 0)
+    {
+        opcode = 51;
+        funct3 = 5;
+        funct7 = 0;
+    }else if (strcmp(opr, "xor") == 0)
+    {
+        opcode = 51;
+        funct3 = 4;
+        funct7 = 0;
+    }else if (strcmp(opr, "or") == 0)
+    {
+        opcode = 51;
+        funct3 = 6;
+        funct7 = 0;
+    }else if (strcmp(opr, "and") == 0)
+    {
+        opcode = 51;
+        funct3 = 7;
+        funct7 = 0;
     }
 
     char *reg = strtok(NULL, ", ");
@@ -77,6 +125,117 @@ void parseRType(char *opr, Instruction *instr)
     instr->instruction |= (rs_1 << (7 + 5 + 3));
     instr->instruction |= (rs_2 << (7 + 5 + 3 + 5));
     instr->instruction |= (funct7 << (7 + 5 + 3 + 5 + 5));
+}
+
+void parseIType(char *opr, Instruction *instr){
+    instr->instruction = 0;
+    unsigned opcode = 0;
+    unsigned funct3 = 0;
+    unsigned funct7 = 0;
+    unsigned rd = 0;
+    unsigned rs_1 = 0;
+    unsigned immediate = 0;
+
+    if(strcmp(opr, "ld") == 0){
+        opcode = 3;
+        funct3 = 3;
+
+        char *reg = strtok(NULL, ", ");
+        rd = regIndex(reg);
+
+        reg = strtok(NULL, "(");
+        immediate = atoi(reg);
+
+        reg = strtok(NULL, ")");
+        rs_1 = regIndex(reg);
+
+    }else{
+        if (strcmp(opr, "addi") == 0)
+        {
+            opcode = 19;
+            funct3 = 0;
+        }else if(strcmp(opr, "slli") == 0){
+            opcode = 19;
+            funct3 = 1;
+        }else if(strcmp(opr, "xori") == 0){
+            opcode = 19;
+            funct3 = 4;
+        }else if(strcmp(opr, "srli") == 0){
+            opcode = 19;
+            funct3 = 5;
+        }else if(strcmp(opr, "ori") == 0){
+            opcode = 19;
+            funct3 = 6;
+        }else if(strcmp(opr, "andi") == 0){
+            opcode = 19;
+            funct3 = 7;
+        }
+        char *reg = strtok(NULL, ", ");
+        rd = regIndex(reg);
+
+        reg = strtok(NULL, ", ");
+        rs_1 = regIndex(reg);
+
+        reg = strtok(NULL, ",");
+        reg[strlen(reg)-1] = '\0';
+        immediate = atoi(reg);
+    }
+    
+    // Contruct instruction
+    instr->instruction |= opcode;
+    instr->instruction |= (rd << 7);
+    instr->instruction |= (funct3 << (7 + 5));
+    instr->instruction |= (rs_1 << (7 + 5 + 3));
+    instr->instruction |= (immediate << (7 + 5 + 3 + 5));
+}
+
+void parseSBType(char *opr, Instruction *instr){
+    instr->instruction = 0;
+    unsigned opcode = 0;
+    unsigned funct3 = 0;
+    unsigned funct7 = 0;
+
+    if (strcmp(opr, "bne") == 0){
+        opcode = 99;
+        funct3 = 1;
+    }
+
+    char *reg = strtok(NULL, ", ");
+    unsigned rs_1 = regIndex(reg);
+
+    reg = strtok(NULL, ", ");
+    unsigned rs_2 = regIndex(reg);
+
+    reg = strtok(NULL, ",");
+    reg[strlen(reg)-1] = '\0';
+    unsigned immediate = atoi(reg);
+
+    unsigned imm_12_10_5 = immediate & 6112;
+    imm_12_10_5 = imm_12_10_5 >> 5;
+
+    unsigned imm_12_10_5_1 = imm_12_10_5 << 1;
+
+    imm_12_10_5 = imm_12_10_5 >> 7;
+
+    imm_12_10_5_1 += imm_12_10_5;
+    imm_12_10_5 = imm_12_10_5_1;
+
+    unsigned imm_4_1_11 = immediate & 2078;
+    imm_4_1_11 = imm_4_1_11 >> 1;
+
+    unsigned imm_4_1_11_1 = imm_4_1_11 << 1;
+
+    imm_4_1_11 = imm_4_1_11 >> 10;
+
+    imm_4_1_11_1 += imm_4_1_11;
+
+    // Contruct instruction
+    instr->instruction |= opcode;
+    instr->instruction |= (imm_4_1_11_1 << 7);
+    instr->instruction |= (funct3 << (7 + 5));
+    instr->instruction |= (rs_1 << (7 + 5 + 3));
+    instr->instruction |= (rs_2 << (7 + 5 + 3 + 5));
+    instr->instruction |= (imm_12_10_5 << (7 + 5 + 3 + 5 + 5));
 }
 
 int regIndex(char *reg)
